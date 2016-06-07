f6ac5d88b5c996678d9d70900c7d59d2cf6dba4e
Sahlre <shlondon8@gmail.com>
2016-06-07 16:14:33 -0500

    modificate plot3.R and plot3.png files

--- plot3.R
+++ plot3.R
@@ -37,8 +37,7 @@ plot(bdfeb$Time1, bdfeb$Sub_metering_1, type = "l", ylab = "Energy sub metering"
 lines(bdfeb$Time1, bdfeb$Sub_metering_2, type = "l", col="red")
 lines(bdfeb$Time1, bdfeb$Sub_metering_3, type = "l", col="blue")
 legend("topright",lty = c(1,1,1), col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       cex = 1)
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #Crea una copia de Plot 3 en formato png
 #Create a copy of Plot 3 in png format
